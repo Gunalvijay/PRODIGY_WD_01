dc971ca90c5d07c0a4e867c9b653530d71694e3e
Gunalvijay <144464847+Gunalvijay@users.noreply.github.com>
2024-08-26 11:22:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+window.addEventListener('scroll', function() {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+function toggleMenu() {
+    const navbar = document.getElementById('navbar');
+    navbar.classList.toggle('active');
+}
